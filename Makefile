e559d40c607cfdd77772e680862080b5b0470a13
hannah <hannah.ellis@pulsic.com>
2020-04-15 08:35:03 +0100

    Adding the real time clock. Also making the code compile as c++11 code

--- Makefile
+++ Makefile
@@ -15,10 +15,10 @@ main.hex: main.elf
 	avr-objcopy -j .text -j .data -O ihex main.elf main.hex
 
 main.elf: main.o
-	avr-g++ -mmcu=$(MCU) -DF_CPU=$(F_CPU) -DBAUD=$(BAUD) -Os main.o -o main.elf
+	avr-g++ -std=c++0x -mmcu=$(MCU) -DF_CPU=$(F_CPU) -DBAUD=$(BAUD) -Os main.o -o main.elf
 
 main.o: main.cpp
-	avr-g++ -mmcu=$(MCU) -DF_CPU=$(F_CPU) -DBAUD=$(BAUD) -Wall -Os -c main.cpp
+	avr-g++ -std=c++0x -mmcu=$(MCU) -DF_CPU=$(F_CPU) -DBAUD=$(BAUD) -Wall -Os -c main.cpp
 
 upload: main.hex
 	avrdude -p $(MCU) -c $(PROGRAMMER_TYPE) -P $(PORT) -b $(BAUD) -U flash:w:main.hex
--- main.cpp
+++ main.cpp
@@ -21,29 +21,6 @@ static const uint8_t numberLookup[] = {
   0b01110001, //F
 };
 
-/*class Display
-{
-private:
-  const volatile uint8_t * port_;
-  uint8_t mosi_;
-  uint8_t clk_;
-  uint8_t ce_;
-public:
-  Display(const volatile uint8_t * port ,
-          const uint8_t mosi ,
-          const uint8_t clk
-          const ce )
-          : port_(port),
-            mosi_(mosi),
-            clk_(clk),
-            ce_(ce){}
-
-  void raw(const uint8_t &x) const
-  {
-
-  }
-};*/
-
 //Some fixed rules about an spi port
 //We use just the lower nibble for data.
 //clk is pin 0 of the port
@@ -106,6 +83,104 @@ static uint8_t shiftIn(volatile uint8_t *port,
   return x ;
 }
 
+class BCDNumber
+{
+  private:
+    uint8_t data_ ;
+  public:
+    BCDNumber( const uint8_t & data = 0 )
+    : data_( data ) {}
+
+    const uint8_t & raw() const
+    {
+      return data_ ;
+    }
+
+    uint8_t units() const
+    {
+      return data_ & 0b00001111 ;
+    }
+
+    uint8_t tens() const
+    {
+      return ( data_ & 0b11110000 ) >> 4 ;
+    }
+
+    uint8_t asuint8_t() const
+    {
+      return units() + 10 * tens() ;
+    }
+};
+
+class RTCData
+{
+  public:
+    BCDNumber seconds ;
+    BCDNumber minutes ;
+    BCDNumber hours ;
+    BCDNumber hoursRaw ;
+    bool twentyFourHour ;
+    bool pm ;
+    uint8_t day ;
+    BCDNumber date ;
+    BCDNumber month ;
+    BCDNumber year ;
+
+    RTCData(const uint8_t (&xs)[7])
+    : seconds( xs[0] ) ,
+      minutes( xs[1] ) ,
+      hoursRaw( xs[2] ),
+      day( xs[3] ) ,
+      date( xs[4] ) ,
+      month( xs[5] ),
+      year( xs[6] )
+    {
+
+      twentyFourHour = (0b01000000 & xs[2] ) != 0 ;
+      if( twentyFourHour )
+      {
+        pm = false ;
+      }
+      else
+      {
+        pm = ( 0b00100000 & xs[2] ) != 0 ;
+      }
+      const uint8_t hourBCD = twentyFourHour ?
+                        xs[2] & 0b00111111 :
+                        xs[2] & 0b00011111 ;
+      hours = BCDNumber( hourBCD ) ;
+    }
+};
+
+class RealTimeClock
+{
+private:
+  volatile uint8_t * out_port_ ;
+  const volatile uint8_t * in_port_ ;
+  uint8_t ce_mask_high_ ;
+  uint8_t ce_mask_low_ ;
+
+public:
+  RealTimeClock(volatile uint8_t * out_port , const volatile uint8_t * in_port , const uint8_t ce_pin )
+  : out_port_( out_port ) , in_port_(in_port) , ce_mask_high_( 1 << ce_pin ) , ce_mask_low_( ~ce_mask_high_ ){}
+
+  RTCData read() const
+  {
+    //start set ce high
+    *out_port_ = *out_port_ | ce_mask_high_ ;
+    //write out instruction, in this case read
+    shiftOut(out_port_,0b00000000,true);
+    uint8_t xs[7]={0,0,0,0,0,0,0};
+    for(unsigned int i = 0 ; i < 7 ; ++i)
+    {
+      xs[i] = shiftIn(out_port_ , in_port_ , true) ;
+    }
+    //stop set ce low
+    *out_port_ = *out_port_ & ce_mask_low_ ;
+    return RTCData( xs ) ;
+  }
+};
+
 class Display
 {
 private:
@@ -132,23 +207,55 @@ public:
   }
 };
 
-static uint8_t readButtons()
+class Buttons
 {
-  //to enable the chip drop chip enable low
-  PORTB = PORTB & 0b11101111; //set ce low
-  //to store the current state of the pins, send mosi low then clock once
-  PORTB = PORTB & 0b11101101; //set mosi low
-  //flash clock up to load the memory
-  PORTB = PORTB | 0b00000001 ; // set clock high
-  PORTB = PORTB & 0b11111110 ; // set clock low
-  //data is now loaded for shifing out
-  //put into shift out mode
-  PORTB = PORTB | 0b00000010 ; //set mosi high
-  //now shift in
-  const uint8_t x = shiftIn(&PORTB,&PINB);
-  //stop
-  PORTB = PORTB | 0b00010000;
-  return x ;
+private:
+  volatile uint8_t * out_port_ ;
+  const volatile uint8_t * in_port_ ;
+  uint8_t ce_mask_high_ ;
+  uint8_t ce_mask_low_ ;
+
+public:
+  Buttons(volatile uint8_t * out_port , const volatile uint8_t * in_port , const uint8_t ce_pin )
+  : out_port_( out_port ) , in_port_(in_port) , ce_mask_high_( 1 << ce_pin ) , ce_mask_low_( ~ce_mask_high_ ){}
+
+  uint8_t read() const
+  {
+    //to enable the chip drop chip enable low
+    *out_port_ = *out_port_ & ce_mask_low_; //set ce low
+    //to store the current state of the pins, send mosi low then clock once
+    *out_port_ = *out_port_ & 0b11101101; //set mosi low
+    //flash clock up to load the memory
+    *out_port_ = *out_port_ | 0b00000001 ; // set clock high
+    *out_port_ = *out_port_ & 0b11111110 ; // set clock low
+    //data is now loaded for shifing out
+    //put into shift out mode
+    *out_port_ = *out_port_ | 0b00000010 ; //set mosi high
+    //now shift in
+    const uint8_t x = shiftIn(out_port_,in_port_);
+    //stop
+    *out_port_ = *out_port_ | ce_mask_high_;
+    return x ;
+  }
+};
+
+void displayTime(const RTCData & td , const Display & disp )
+{
+  disp.raw(0);
+  disp.show(td.hours.tens());
+  _delay_ms(500);
+  disp.raw(0);
+  disp.show(td.hours.units());
+  _delay_ms(500);
+  disp.raw(0);
+  _delay_ms(1000);
+  disp.raw(0);
+  disp.show(td.minutes.tens());
+  _delay_ms(500);
+  disp.raw(0);
+  disp.show(td.minutes.units());
+  _delay_ms(1000);
+  disp.raw(0);
 }
 
 int main()
@@ -158,14 +265,15 @@ int main()
   //       43210ssk
   //            oi
   DDRB = 0b11111011;
-  Display disp(&PORTB,3);
+  PORTB = 0b0010000;
+  const auto disp = Display(&PORTB,3);
+  const auto buttons = Buttons(&PORTB,&PINB,4);
+  const auto rtc = RealTimeClock(&PORTB,&PINB,5);
   disp.raw(0x00);
-
-  PORTB = 0 ;
   while(true)
   {
-    const uint8_t buttons = readButtons() ;
-    if( buttons == 0b10000000 )
+    const uint8_t value = buttons.read() ;
+    if( value == 0b10000000 )
     {
       for(uint8_t i=0;i<16;++i)
       {
@@ -174,13 +282,18 @@ int main()
       }
       disp.raw(0x00);
     }
-    else if( buttons == 0b01000000 )
+    else if( value == 0b01000000 )
     {
       for(uint8_t i=0;i<16;++i)
       {
         disp.show(15-i);
         _delay_ms(200);
       }
+      disp.raw(0x00);
+    }
+    else if( value == 0b00000001 )
+    {
+      displayTime(rtc.read(),disp);
       disp.raw(0x00);
     }
   }
